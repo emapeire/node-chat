7899419709edfe15738b671b35a22d4279e729a2
Emanuel Peire <emapeire@outlook.com>
2023-12-16 17:58:30 -0300

    Add logger and disable x-powered-by

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,11 @@
 import express from 'express'
+import logger from 'morgan'
 
 const port = process.env.PORT ?? 3000
 
 const app = express()
+app.use(logger('dev'))
+app.disable('x-powered-by')
 
 app.get('/', (req, res) => {
   res.send('Hello, World!')
