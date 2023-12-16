c4296e971f3a6437430cff6a63b5dc74c7896450
Emanuel Peire <emapeire@outlook.com>
2023-12-16 17:50:35 -0300

    Update package.json and package-lock.json with license and description

--- src/server/index.js
+++ src/server/index.js
@@ -1,1 +1,13 @@
 import express from 'express'
+
+const port = process.env.PORT ?? 3000
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!')
+})
+
+app.listen(port, () => {
+  console.log(`Server listening on port http://localhost:${port}`)
+})
